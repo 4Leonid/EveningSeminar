7607bb7fad64bef50fc3777e8ca297fabf7afed4
Leonid <leo-turko@mail.ru>
2023-02-28 19:19:56 +0300

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+
+int FindSecond(int num)
+{
+    int firstItaration = num / 10;
+    int result = firstItaration % 10;
+
+    return result;   
+}
+
+Console.Write($"Inter you number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The second number of {number} is: {FindSecond(number)}");
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+
+void FindThird(int num)
+{
+  if (num < 100) 
+  {
+    Console.WriteLine($"У цифры {num} нет треньей цифры");
+   return;
+  }
+   
+  if (num > 999)
+    {
+      num /= 10;
+    }
+    Console.WriteLine($"The third number of {num} is {num % 10}");   
+}
+
+Console.Write($"Inter you number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+FindThird(number);
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+
+*/
+void WeekDay(int num)
+  { 
+    if (num > 7) 
+    {
+      Console.WriteLine("Нет такого дня недели");
+      return;
+    }
+    
+    if (num == 6 || num == 7)
+    {
+      Console.WriteLine($"{num} день это выходной");
+      return;
+    } 
+
+      Console.WriteLine($"{num}-Й день это будний день!");
+  } 
+
+Console.Write($"Inter you week day: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+WeekDay(weekDay);
---
+++ Seminar2/Program.cs
@@ -0,0 +1,99 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int CutNumber(int num)
+{
+    int hundreds = num / 100;
+    int ones = num % 10;
+
+    return hundreds * 10 + ones;
+}
+
+int randomNumber = new Random().Next(100, 1000);
+
+int newNumber = CutNumber(randomNumber);
+
+Console.WriteLine($" New number of {randomNumber} number is {newNumber}");
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+
+bool isDivided(int number, int firstDivider, int secondDivider)
+{
+return number % firstDivider == 0 && number % secondDivider == 0;
+}
+
+Console.Write($"Input a number for checking: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Input a first divider: ");
+int div1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Input a second divider: ");
+int div2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(isDivided(num, div1, div2));
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+
+
+
+
+/*
+int CutNumber( int num)
+{
+    int ferst = num / 10;
+    int second = num % 10;
+  if( ferst > second)
+  {
+    return ferst;
+  }
+  else
+  {
+     return second;
+  }
+   
+}
+
+
+ int randNumber= new Random().Next(10 ,99);
+ int newNum = CutNumber(randNumber);
+
+Console.WriteLine($"For {randNumber} max number is {CutNumber(randNumber)}");
+*/
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+/*
+
+/*
+решение от второго зала
+Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+ */
+
+/*
+int CutNumber( int num)
+{
+    int ferst = num / 10;
+    int second = num % 10;
+  if( ferst > second)
+  {
+    return ferst;
+  }
+  else
+  {
+     return second;
+  }
+   
+}
+
+
+ int randNumber= new Random().Next(10 ,99);
+ int newNum = CutNumber(randNumber);
+
+Console.WriteLine($"For {randNumber} max number is {CutNumber(randNumber)}");
+*/
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+/*
+*/
