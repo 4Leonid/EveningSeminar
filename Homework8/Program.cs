301650339331830dac0b4e5b8ca5149497396b62
Leonid <leo-turko@mail.ru>
2023-03-18 13:47:03 +0300

    Homework tasks

---
+++ Homework8/Program.cs
@@ -0,0 +1,161 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+double[,] FillMatrixWithRandom(int row, int column)
+{
+    double[,] array = new double[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double num = rnd.NextDouble() + rnd.Next(-10, 10);
+            array[i, j] = Math.Round(num, 2);
+        }
+    }
+    return array;
+}
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] matrix, int num)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == num)
+        {
+            Console.Write($"{num} есть в массиве");
+            return;
+        }
+        }
+    }
+    Console.Write($"Числа {num} в массиве нет");
+    return;
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+Random rnd = new Random();
+int number = rnd.Next(0,11);
+Console.Write($"Найти номер {number}");
+Console.WriteLine();
+PrintMatrix(matrix);
+FindNumber(matrix, number);
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumRows(int[,] matrix, double rows)
+{
+    int sum = 0;
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            sum += matrix[j, i];
+        }
+        avarage = Math.Round(sum / rows, 2);
+        Console.WriteLine($"Average of column {i + 1}: {avarage}");
+        sum = 0;
+    }
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+SumRows(matrix, row);
+*/
