2d7869a8f82a4a8f6c19da1e263df7ddc921a20d
Leonid <leo-turko@mail.ru>
2023-03-18 13:48:35 +0300

    Make some copys

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -55,7 +55,7 @@ Console.WriteLine($"Qty of numbers > 0 is {result}");
 ////
 Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-*/
+
 
 int coef = 0;
 int constant = 1;
@@ -115,3 +115,19 @@ if (ValidateLines(lineData1, lineData2))
     Console.Write($"Point of cross y = {lineData1[coef]} * x + {lineData1[constant]} and y = {lineData2[coef]} * x + {lineData2[constant]} ");
     Console.WriteLine($" has coordinates ({coord[x_coord]}), {coord[y_coord]})");
 }
+*/
+
+// Второе решение:
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
